1aa80fcd98de3ade78685947e40cee0014cece4d
E. H. Rast <erich@snafu.de>
2020-03-05 15:15:14 +0000

    Changed the API to use a nested structure. This is unfortunate but a requirement for my needs.

--- hooks.go
+++ hooks.go
@@ -7,42 +7,67 @@ import "sync"
 type HookFunc func(a []interface{}) (interface{}, error)
 
 var lock sync.RWMutex
-var cb map[int]HookFunc
+var cb map[int]map[int]HookFunc
+var counter int
 
-// Set stores the given hook for the ID. If a function already exists under that ID, it is overwritten.
-func Set(id int, f HookFunc) {
+// Add a function for the given hook. The function is added to the list of functions of the hook.
+// Add returns an ID2 that represents the individual function of the hook.
+func Add(hook int, f HookFunc) int {
 	lock.Lock()
 	defer lock.Unlock()
-	cb[id] = f
+	hooks, ok := cb[hook]
+	if !ok || hooks == nil {
+		hooks = make(map[int]HookFunc)
+	}
+	counter++
+	hooks[counter] = f
+	cb[hook] = hooks
+	return counter
 }
 
-// Exec executes the hook for given ID and arguments, if there is any. It does nothing if there is none.
-func Exec(id int, args ...interface{}) {
+// Exec executes all functions for the hook with the given args. It does nothing if there is no function
+// for the hook. The functions for a hook are executed in arbitrary order.
+func Exec(hook int, args ...interface{}) {
 	lock.RLock()
 	defer lock.RUnlock()
-	if f, ok := cb[id]; ok {
+	hooks, ok := cb[hook]
+	if !ok || hooks == nil {
+		return
+	}
+	for _, f := range hooks {
 		f(args)
 	}
 }
 
-// Remove the given hook, if it exists.
-func Remove(id int) {
+// Remove the function with ID for the given hook, if it exists.
+func Remove(hook, id int) {
 	lock.Lock()
 	defer lock.Unlock()
-	delete(cb, id)
+	hooks, ok := cb[hook]
+	if !ok || hooks == nil {
+		return
+	}
+	delete(hooks, id)
 }
 
-// Active returns true if the given hook is set, false otherwise. Using this first before using Exec
+// Remove all functions for the hook.
+func RemoveAll(hook int) {
+	lock.Lock()
+	defer lock.Unlock()
+	delete(cb, hook)
+}
+
+// Active returns true if the hook is set, false otherwise. Using this first before using Exec
 // may be more efficient due to the arguments provided to Exec.
-func Active(id int) bool {
+func Active(hook int) bool {
 	lock.RLock()
 	defer lock.RUnlock()
-	_, ok := cb[id]
+	_, ok := cb[hook]
 	return ok
 }
 
 func init() {
 	lock.Lock()
 	defer lock.Unlock()
-	cb = make(map[int]HookFunc)
+	cb = make(map[int]map[int]HookFunc)
 }
